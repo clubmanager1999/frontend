50c6bead2d6eba15e2624ce6048b3b9538f0e93d
Don Kanaille <149098194+donkanaille42@users.noreply.github.com>
2023-11-25 20:30:57 +0100

    Add oidc login (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import './App.css';
+import { useAuth } from 'react-oidc-context';
+
+function Auth() {
+  const auth = useAuth();
+
+  switch (auth.activeNavigator) {
+    case 'signinSilent':
+      return <div>Signing in...</div>;
+    case 'signoutRedirect':
+      return <div>Signing out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Failed to login: {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    console.log(auth.user);
+    return (
+      <div>
+        Hello {auth.user?.profile.preferred_username}
+        <button onClick={() => void auth.removeUser()}>Log out</button>
+      </div>
+    );
+  }
+
+  return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
+}
 
 function App() {
-  return <div className="App">Hi</div>;
+  return <div className="App">{Auth()}</div>;
 }
 
 export default App;
