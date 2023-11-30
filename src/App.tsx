194b88a1fe2dfbc5d20a0bd9c723dee54eac229b
Don Kanaille <149098194+donkanaille42@users.noreply.github.com>
2023-11-30 17:23:51 +0100

    Add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,90 @@
 import './App.css';
 import { useAuth } from 'react-oidc-context';
-import { Button } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import LogoutIcon from '@mui/icons-material/Logout';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Auth from './auth/Auth';
+
+const drawerWidth = 180;
 
 function App() {
   const auth = useAuth();
+
   return (
     <Auth>
-      <div className="App">
-        Hello {auth.user?.profile.preferred_username}
-        <Button
-          onClick={() => void auth.removeUser()}
-          variant="contained"
-          startIcon={<LogoutIcon />}
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
         >
-          Log out
-        </Button>
-      </div>
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Home
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        >
+          <Drawer
+            variant="permanent"
+            sx={{
+              '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+              },
+            }}
+            open
+          >
+            <Toolbar />
+            <Divider />
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <LogoutIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Log out"
+                    onClick={() => auth.removeUser()}
+                  />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Drawer>
+        </Box>
+
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+          }}
+        >
+          <Toolbar />
+          <Typography paragraph>
+            Hello {auth.user?.profile.preferred_username}
+          </Typography>
+        </Box>
+      </Box>
     </Auth>
   );
 }
