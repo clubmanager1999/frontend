47258e206e485bc639adc6e4a19034787994b950
Don Kanaille <149098194+donkanaille42@users.noreply.github.com>
2023-12-12 19:35:14 +0100

    Centralize route infos

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,9 @@ import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from 'react-oidc-context';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './home/Home.tsx';
-import Profile from './profile/Profile.tsx';
-import MemberList from './member/MemberList.tsx';
-import MemberDetail from './member/MemberDetail.tsx';
 import ApiClientProvider from './api/ApiClientProvider.tsx';
 import Auth from './auth/Auth.tsx';
-import MembershipList from './membership/MembershipList.tsx';
-import MembershipDetail from './membership/MemberShipDetail.tsx';
-import CreditorList from './creditor/CreditorList.tsx';
-import CreditorDetail from './creditor/CreditorDetail.tsx';
-import DonorDetail from './donor/DonorDetail.tsx';
-import DonorList from './donor/DonorList.tsx';
+import { routes } from './routes.tsx';
 
 const config = {
   authority: import.meta.env.VITE_OIDC_AUTHORITY,
@@ -27,48 +18,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/profile',
-        element: <Profile />,
-      },
-      {
-        path: '/memberships',
-        element: <MembershipList />,
-      },
-      {
-        path: '/memberships/:id',
-        element: <MembershipDetail />,
-      },
-      {
-        path: '/members',
-        element: <MemberList />,
-      },
-      {
-        path: '/members/:id',
-        element: <MemberDetail />,
-      },
-      {
-        path: '/creditors',
-        element: <CreditorList />,
-      },
-      {
-        path: '/creditors/:id',
-        element: <CreditorDetail />,
-      },
-      {
-        path: '/donors',
-        element: <DonorList />,
-      },
-      {
-        path: '/donors/:id',
-        element: <DonorDetail />,
-      },
-    ],
+    children: routes,
   },
 ]);
 
